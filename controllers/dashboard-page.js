a14f684aed78caf8e4f3c672f1532abaa9351347
Tyrion Lannister <win.pomerantz@gmail.com>
2024-05-06 11:40:43 -0700

    resolved dashboard not logged in view

--- controllers/dashboard-page.js
+++ controllers/dashboard-page.js
@@ -3,32 +3,34 @@ const { Post, User, Comment } = require('../models');
 
 router.get('/', async (req, res) => { 
   try {
+    if (!req.session.user_id) {
+      // Handle case where user is not logged in
+      res.render('dashboard', { message: 'Please log in to view the dashboard' });
+      return;
+    }
+
     const userData = await User.findByPk(req.session.user_id);
     const postData = await Post.findAll({
       where: {
         user_id: req.session.user_id,
       },
-      include: [{model: Comment, include: [{model: User}]}, {model: User}]
+      include: [{ model: Comment, include: [{ model: User }] }, { model: User }],
     });
 
-    const user = userData.get({
-      plain: true,
-    })
-
+    const user = userData.get({ plain: true });
     const posts = postData.map((post) => post.get({ plain: true }));
-    console.log('HERE')
-    console.log(userData);
 
     res.render('dashboard', {
       user,
       posts,
-      loggedIn: true,
+      loggedIn: req.session.loggedIn,
     });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
 });
+
 
 // Renders the new post page
 router.get('/new', async (req, res) => {
